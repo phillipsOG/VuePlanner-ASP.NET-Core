95101b8bf7fd776ddfc46d31baed14f8bd5c09bc
C2ierce <c2ierce@gmail.com>
2022-10-15 21:07:35 +0100

    Added entity framework and northwind .db sample data

---
+++ Models/CustomerDemographic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using NorthwindData.Models;
+
+namespace NorthwindData;
+
+public partial class CustomerDemographic
+{
+    public string CustomerTypeId { get; set; } = null!;
+
+    public string? CustomerDesc { get; set; }
+
+    public virtual ICollection<Customer> Customers { get; } = new List<Customer>();
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using NorthwindData;
+
 namespace VuePlanner
 {
     public class Program
     {
+        public static string result = "";
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
@@ -30,6 +34,21 @@ namespace VuePlanner
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
+            
+            #region READ
+            using (var db = new NorthwindContext())
+            {
+                result = db.ContextId.ToString();
+                Console.WriteLine($"[DB_ID][{db.ContextId}]");
+                foreach (var con in db.Customers)
+                {
+                    //Console.WriteLine($"[CustomerID][{con.CustomerId}][Name][{con.ContactName}][City][{con.City}]");
+                    //Console.WriteLine(con.ToString());
+                }
+                //db.Customers.ToList().ForEach(c => Console.WriteLine(c));
+            }
+            #endregion
+            
             app.Run();
         }
     }
