f71ab39c231092f2ece27d1bdbb8fec20689632d
C2ierce <c2ierce@gmail.com>
2022-10-15 22:13:10 +0100

    Really fixed NUnit this time...

--- VuePlannerTests/UnitTest1.cs
+++ VuePlannerTests/UnitTest1.cs
@@ -1,18 +1,17 @@
 using NUnit.Framework;
 
-namespace VuePlannerTests
-{
-    public class Tests
-    {
-        [SetUp]
-        public void Setup()
-        {
-        }
+namespace VuePlannerTests;
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
     }
 }
